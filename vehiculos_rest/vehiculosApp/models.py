a92123a98f181ccdaa36d5e32fbc0bfe71a08f22
Daniel Hurtado <dehurtado@unbosque.edu.co>
2023-03-24 00:00:18 -0500

    FPPG-50 #Metodos DELETE y PUT de Vehiculos

--- vehiculos_rest/vehiculosApp/models.py
+++ vehiculos_rest/vehiculosApp/models.py
@@ -2,8 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Vehiculo(models.Model):
-    placa = models.CharField('Placa identificada',max_length=6, unique=True, null=False)
-    img_placa = models.ImageField('Imagen Placa', upload_to='placas/', max_length=255, unique=True, null=False)
+    placa = models.CharField('Placa identificada',max_length=10, unique=True)
+    img_placa = models.ImageField('Imagen Placa', upload_to='placas/', max_length=255, unique=True, null=True)
     hora_ingreso = models.TimeField('Hora de ingreso', auto_now_add=True, null=False)
     hora_salida = models.TimeField('Hora de salida', auto_now_add=True, null=False)
     salida = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.placa} {self.hora_ingreso}'
