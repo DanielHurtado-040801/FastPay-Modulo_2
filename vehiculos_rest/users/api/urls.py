6592cc410ef7d1a22b11484471a2e789d371ca3f
Daniel Hurtado <dehurtado@unbosque.edu.co>
2023-03-23 23:29:07 -0500

    FPPG-45 #Metodos PUT y DELETE usuarios

--- vehiculos_rest/users/api/urls.py
+++ vehiculos_rest/users/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from users.api.api import user_api_view
+from users.api.api import user_api_view, user_detail_view
 
 urlpatterns = [
-    path('usuario/', user_api_view, name='usuario_api')
+    path('usuario/', user_api_view, name='usuarios_api'),
+    path("usuario/<int:pk>/", user_detail_view, name="Usuario_detail_view")
 ]
