e9440319a4b069d82d655849381611939cf25dce
Daniel Hurtado <dehurtado@unbosque.edu.co>
2023-05-23 23:20:40 -0500

    FPPG-116

--- vehiculos_rest/vehiculosApp/models.py
+++ vehiculos_rest/vehiculosApp/models.py
@@ -5,7 +5,7 @@ from datetime import datetime
 # Create your models here.
 class Vehiculo(BaseModel):
     placa = models.CharField('Placa identificada',max_length=10, unique=True)
-    img_placa = models.ImageField('Imagen Placa', upload_to='vehiculos_rest/vehiculos_rest/img/placas/', max_length=255, null=True)
+    img_placa = models.ImageField('Imagen Placa', upload_to='placas/', max_length=255, null=True)
     hora_ingreso = models.DateTimeField('Hora de ingreso', auto_now_add=False, null=False)
     hora_pago = models.DateTimeField('Hora de pago', null=True, auto_now_add=False)
     valor_pagar = models.DecimalField(max_digits=8, decimal_places=2, null=True, default=0)
