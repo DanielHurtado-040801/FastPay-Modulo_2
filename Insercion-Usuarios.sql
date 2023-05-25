478b1d4a5a9b991f7c4c1e46f608b28fd2e6f601
Daniel Hurtado <dehurtado@unbosque.edu.co>
2023-05-25 00:21:31 -0500

    FPPG-127

---
+++ Insercion-Usuarios.sql
@@ -0,0 +1,13 @@
+-- SQLite
+INSERT INTO users_user (username, email, name, last_name, password, is_active, is_staff, is_superuser)
+VALUES
+    ('usuario1', 'usuario1@example.com', 'Nombre1', 'Apellido1', 'contraseña1', 1, 1, 0),
+    ('usuario2', 'usuario2@example.com', 'Nombre2', 'Apellido2', 'contraseña2', 1, 1, 0),
+    ('usuario3', 'usuario3@example.com', 'Nombre3', 'Apellido3', 'contraseña3', 1, 1, 0),
+    ('usuario4', 'usuario4@example.com', 'Nombre4', 'Apellido4', 'contraseña4', 1, 1, 0),
+    ('usuario5', 'usuario5@example.com', 'Nombre5', 'Apellido5', 'contraseña5', 1, 1, 0),
+    ('usuario6', 'usuario6@example.com', 'Nombre6', 'Apellido6', 'contraseña6', 1, 1, 0),
+    ('usuario7', 'usuario7@example.com', 'Nombre7', 'Apellido7', 'contraseña7', 1, 1, 0),
+    ('usuario8', 'usuario8@example.com', 'Nombre8', 'Apellido8', 'contraseña8', 1, 1, 0),
+    ('usuario9', 'usuario9@example.com', 'Nombre9', 'Apellido9', 'contraseña9', 1, 1, 0),
+    ('usuario10', 'usuario10@example.com', 'Nombre10', 'Apellido10', 'contraseña10', 1, 1, 0);
